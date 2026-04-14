506ab264f209eb56537c0d07fa2fb559d19f5bc2
Alvaro <aossesa@gmail.com>
2026-04-14 01:58:39 -0400

    Configurar API URL por entorno

--- mockup-prototipo/src/config/api.js
+++ mockup-prototipo/src/config/api.js
@@ -1,3 +1,9 @@
 const RAW_API_URL = import.meta.env.VITE_API_URL || '/api'
 
-export const API_URL = RAW_API_URL.replace(/\/$/, '')
+const normalized = RAW_API_URL.startsWith('/')
+  ? RAW_API_URL
+  : RAW_API_URL.startsWith('http://') || RAW_API_URL.startsWith('https://')
+    ? RAW_API_URL
+    : `https://${RAW_API_URL}`
+
+export const API_URL = normalized.replace(/\/$/, '')
